e804837063ddd3cf74fd6ab3ce8df916601cb808
Donnie Holmes <donnieh@gmail.com>
2020-06-01 13:20:00 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+NAME = "nuget-package-scanner"
+VERSION = "0.0.1"
+REQUIRES = [
+    "aiohttp>=3,<4",
+    "cchardet>=2.1.6",
+    "aiodns>=2.0.0",
+    "async-lru>=1.0.2",
+    "lxml>=4.5.1",
+    "tenacity>=6.2.0",
+]
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author="Donnie Holmes",
+    author_email="donnieh@gmail.com",
+    description="Scans a github org for Nuget dependencies and builds a currency report",
+    keywords="nuget github org dependencies currency report",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doneholmes/nuget-package-scanner",
+    install_requires=REQUIRES,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.2',
+)

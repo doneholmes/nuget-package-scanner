cf64cbea2c63c8328f5a599c171d775f14ad2d13
Donnie Holmes <donnieh@gmail.com>
2020-06-02 10:14:00 -0500

    Bumping release again for testing

--- nuget_package_scanner/app.py
+++ nuget_package_scanner/app.py
@@ -13,7 +13,7 @@ from nuget_package_scanner.github_search import GithubClient, GithubSearchResult
 from nuget_package_scanner.nuget import NetCoreProject, Nuget, Package, PackageConfig, PackageContainer
 
 NAME = 'nuget-package-scanner'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 def enable_console_logging(level: int = logging.INFO):   
     logger = logging.getLogger()
